405712252b22fdaaf5a06efb3bd9e0ce862077f3
jfulse <jorgen.fulsebakke@gmail.com>
2021-04-07 20:11:58 +0200

    :bug: Avoid react state error

--- src/hooks/useAuthenticatedData.js
+++ src/hooks/useAuthenticatedData.js
@@ -3,11 +3,13 @@ import { useCallback, useEffect, useState } from 'react';
 import useAutheticatedFetch from './useAutheticatedFetch';
 
 export default (url, { method } = { method: 'GET' }) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
   const authenticatedFetch = useAutheticatedFetch();
 
   const fetchData = useCallback(async () => {
     const response = await authenticatedFetch(url, { method });
+
+    if (response.status !== 200) return;
 
     try {
       setData({ ...response, body: await response.json() });
