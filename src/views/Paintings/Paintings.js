09e3403b5f712d167b4d91f2a4403e6061b192e2
SaioAU <saioarostegi+me@gmail.com>
2021-07-08 17:01:51 +0200

    added component  paintins into paitings page  with filter on category

--- src/views/Paintings/Paintings.js
+++ src/views/Paintings/Paintings.js
@@ -1,7 +1,23 @@
-const Paintings = () => (
-  <div>
-    <div>paintings</div>
-  </div>
-);
+import { useData } from '../../hooks';
+import { Product } from '../../components';
+
+const Paintings = () => {
+  const { data, loading, error } = useData('products/read/all');
+  console.log(data);
+  // console.log(data, 'here is my data');
+  if (loading) return <div> loading </div>;
+  if (error) return <div> error </div>;
+  const paintings = data.filter(function (item) {
+    return item.category === 'painting';
+  });
+
+  return (
+    <div>
+      {paintings.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
 
 export default Paintings;
--- src/views/Seasons/Seasons.js
+++ src/views/Seasons/Seasons.js
@@ -2,8 +2,6 @@ import { Switch, Route } from 'react-router-dom';
 import { Season } from './components';
 
 const Seasons = () => {
-  console.log('HERE🇹🇿');
-  // const seasons = fetchSeasons();
   return (
     <Switch>
       <Route path="/seasons/:seasonId">
