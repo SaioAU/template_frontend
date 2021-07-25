9a373ab63a3e5154bd9251dc2575413d46f7f186
SaioAU <saioarostegi+me@gmail.com>
2021-07-25 15:31:43 +0200

    css to products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,10 @@
 import { ProductType } from '../../propTypes';
+import styles from './Product.scss';
 
 const Product = ({ product }) => (
-  <li>
+  <li className={styles.product}>
     {product.name}
+    <img src="https://i.insider.com/5484d9d1eab8ea3017b17e29?width=600&format=jpeg&auto=webp" />
     <p>category:{product.category}</p>
     <p>colour: {product.colour}</p>
     <p>description: {product.description}</p>
